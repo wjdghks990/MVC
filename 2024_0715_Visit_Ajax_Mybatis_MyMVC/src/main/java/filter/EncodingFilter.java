a30b809e49cac7697e7ce73e9647add860a04297
wjdghks990 <qnfxksmstkwk@naver.com>
2024-07-16 18:16:44 +0900

    MVC 활용해서 방명록 예제 구현

---
+++ 2024_0715_Visit_Ajax_Mybatis_MyMVC/src/main/java/filter/EncodingFilter.java
@@ -0,0 +1,57 @@
+package filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class EncodingFilter
+ */
+
+//@WebFilter("*.do") // web.xml에서 <filter>로 해결할 수도 있음
+public class EncodingFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public EncodingFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(
+			ServletRequest  request, 
+			ServletResponse response, 
+			FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+        request.setCharacterEncoding("utf-8");
+        
+        //System.out.println("--doFilter() call--");
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
